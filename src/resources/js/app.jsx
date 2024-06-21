c8f4048319af875f9902cc9893534579c6efb6eb
SAKAMOTO <watskmt@gmail.com>
2024-06-21 15:38:51 +0900

    react インストール

---
+++ src/resources/js/app.jsx
@@ -0,0 +1,24 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes React and other helpers. It's a great starting point while
+ * building robust, powerful web applications using React + Laravel.
+ */
+
+import './bootstrap';
+
+/**
+ * Next, we will create a fresh React component instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+import './components/Example';
+import ReactDOM from "react-dom/client";
+import { render } from 'react-dom';
+
+function App() {
+    return <h1>Hello World!!</h1>;
+}
+
+const root = ReactDOM.createRoot(document.getElementById("app"));
+root.render(<App />);
--- src/vite.config.js
+++ src/vite.config.js
@@ -1,17 +1,26 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/sass/app.scss',
+                'resources/js/app.jsx',
+            ],
             refresh: true,
         }),
+        react(),
     ],
     server: {
         host: true,
         hmr: {
+            clientPort: 5173,
             host: 'localhost',
         },
-    },
+        watch: {
+            usePolling: true
+        },
+    }
 });
