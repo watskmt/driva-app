43ba6b693557433483871496539a6ae0e49b1f37
R5-7600\watsk <watskmt@hotmail.com>
2024-08-08 11:37:54 +0900

    設定中

--- driva-app/src/App.tsx
+++ driva-app/src/App.tsx
@@ -7,17 +7,17 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+          <a href="https://vitejs.dev" target="_blank" rel="noopener noreferrer">
+              <img src={viteLogo} className="logo" alt="Vite logo"/>
+          </a>
+          <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+              <img src={reactLogo} className="logo react" alt="React logo"/>
+          </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
+        <h1>Vite + React</h1>
+        <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -28,7 +28,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </div>
   )
 }
 
--- driva-app/src/main.tsx
+++ driva-app/src/main.tsx
@@ -2,6 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_APP_ID,
+};
+
+// @ts-ignore
+const app = initializeApp(firebaseConfig);
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
